edbf2fd86763dc1542e216bdaf9fdde2df8b1d58
AdeelAtta <adeelatta@gmail.com>
2023-02-19 00:33:39 +0500

    Counter app updated

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-#React_Counter
+# Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Available Scripts
+
+In the project directory, you can run:
+
+## step1
+Clone repository
+
+## step2
+npm install
+
+## step3
+npm start
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+
+##This project is open for OpenSource Changes and Improvements
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css';
+
+function App() {
+
+  const [count, setCount] = useState(0);
+
+ function handleClick(){
+    setCount(count + 1);
+  }
+
+  return (
+    <>
+      {[...Array(15)].map((index) => (
+        <div key={index} className='count-box' style={{ top: Math.random() * 400, left: Math.random() * 1000, color: `#${Math.random().toString(16).substr(-6)}` }}>{count}</div>
+      ))}
+
+      <button className='button' onClick={handleClick}>+</button>
+
+    </>
+  );
+}
+
+export default App;
